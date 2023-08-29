a25b6401a61cb38374416ce7be7ac14e0b4e5af2
Danny <dannythompson901@gmail.com>
2023-08-28 22:01:48 -0500

    fix: removing comments and unused lines (WEB-2)

--- app.js
+++ app.js
@@ -2,29 +2,27 @@ import { Client, Account, ID } from 'appwrite';
 
 const client = new Client();
 
-client.setEndpoint('https://cloud.appwrite.io/v1').setProject('project_ID');
+client
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject('64ed48278fdff2ec2135');
 
 const account = new Account(client);
 
-// Initial form data
 const formData = {
   name: '',
   email: '',
   password: '',
 };
 
-// Handle input change
 const handleChange = (event) => {
   const name = event.target.name;
   const value = event.target.value;
   formData[name] = value;
 };
 
-// Handle form submit
 const handleSubmit = (event) => {
   event.preventDefault();
   console.log(formData);
-  // Add your submit logic here, such as sending the form data to a server
   account
     .create(ID.unique(), formData.email, formData.password, formData.name)
     .then(
@@ -37,7 +35,6 @@ const handleSubmit = (event) => {
     );
 };
 
-// Attach event listeners to the form elements
 document.getElementById('name').addEventListener('input', handleChange);
 document.getElementById('email').addEventListener('input', handleChange);
 document.getElementById('password').addEventListener('input', handleChange);
